fa0b5774ccd52dd65a730797f31adc9531b21379
«platonische» <platonische@gmail.com>
2024-06-24 16:01:56 +0400

    [DBW-01] Added new variables, script sync with live

---
+++ bin/sync.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+SCRIPTPATH=$( cd "$(dirname "$0")" ; pwd -P )
+cd $SCRIPTPATH
+ROOTPATH=$( cd "$SCRIPTPATH/.." ; pwd -P )
+cd $ROOTPATH
+
+source ./src/loader.sh
+
+if [[ $SERVER_LIVE eq 1 ]]; then
+  echo "It shouldn't be run on live server"
+  exit 1
+fi
+
+`bash bin/get-live-db.sh 2>&1 \
+&& bash bin/db-restore.sh 2>&1 \
+&& bash bin/db-storage-manager.sh 2>&1`
+
